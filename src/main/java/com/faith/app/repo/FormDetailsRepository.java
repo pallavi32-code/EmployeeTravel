17a96e5ba55f2d8cdd1117cab36e8ea89d154815
pallavi <pallavisasidharantv@gmail.com>
2021-03-30 17:16:51 +0530

    second commit

--- src/main/java/com/faith/app/repo/FormDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.faith.app.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormDetailsRepository extends CrudRepository<FormDetailsRepository, Integer> {
-
-}
